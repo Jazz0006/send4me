977d514c74539399b8df8d076750c5fbaa167cc4
JazzZeng <jazzzeng@LAPTOP-ASUS.localdomain>
2021-07-10 14:06:44 +1000

    Added Virtual Environment and requirements.txt

--- test_client.py
+++ test_client.py
@@ -1,4 +1,8 @@
 import socket
+#import cowsay
+
+#cowsay.stegosaurus("YOU are outstanding!")
+#print(cowsay.get_output_string('trex', "I like a hug, but my arms are too short"))
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect(('192.168.1.106', 5555))
