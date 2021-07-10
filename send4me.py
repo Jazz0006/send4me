a44cae958f1bcc3c21c7c99fac86c0bd56cca064
JazzZeng <jazzzeng@LAPTOP-ASUS.localdomain>
2021-07-10 16:50:09 +1000

    Can send one specified file

--- send4me.py
+++ send4me.py
@@ -13,11 +13,21 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serversocket:
     conn, addr = serversocket.accept()
     with conn:
         print("Connected by", addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+
+        print("Now receiving files")
+        with open("received.svg", "wb") as rf:
+            while True:
+                byte_read = conn.recv(4096)
+                if not byte_read:
+                    break
+                rf.write(byte_read)
+
+                #data = conn.recv(4096)
+                #if not data:
+                #    break
+            #conn.sendall(data)
+    print("Successfully received one file")
+print("close the socket")
 
 
 
--- test_client.py
+++ test_client.py
@@ -4,9 +4,23 @@ import socket
 #cowsay.stegosaurus("YOU are outstanding!")
 #print(cowsay.get_output_string('trex', "I like a hug, but my arms are too short"))
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect(('192.168.1.106', 5555))
-    s.sendall(b'Hello World')
-    data = s.recv(1024)
+BUFFER_SIZE = 4096
 
-print('Received', repr(data))
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect(('192.168.1.105', 9587))
+    print("Connect successfle")
+
+    print("Now opening the file to be sent")
+    with open("send4me.svg", "rb") as f:
+        while True:
+            bytes_read = f.read(BUFFER_SIZE) 
+            if not bytes_read:
+                break
+
+            s.sendall(bytes_read)
+
+    print("Finished sending send4me.svg")
+    #s.sendall(b'Hello World')
+    #data = s.recv(1024)
+print("Close the socket")
+#print('Received', repr(data))
