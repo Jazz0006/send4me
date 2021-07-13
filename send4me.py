5ff36a13c47f2792b2ea53a48ce1cce3563caced
JazzZeng <jazzzeng@LAPTOP-ASUS.localdomain>
2021-07-13 23:05:49 +1000

    Dealing with Errors on sender side

--- send4me.py
+++ send4me.py
@@ -15,15 +15,15 @@ def get_local_ip_address():
     return address
 
 def get_target_ip_address():
-    with open("send4me.ini", "r") as conf:
-        if not conf:
-            return None
-        else:
+    try: 
+        with open("send4me.ini", "r") as conf:
             return conf.readline().strip()
+    except FileNotFoundError:
+        return None
 
 def receive_content():
     ip_address = get_local_ip_address()
-    print(f"This computer's IP address is {ip_address}")
+    print(f"Info:   This computer's IP address is {ip_address}")
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         try:
             s.bind(('', 9587))                        
@@ -34,30 +34,32 @@ def receive_content():
 
         try:
             s.listen()
-            print("Ready to receive files.")
-            print("Waiting for connection...")
-            print(f"To connect and send files, on the other computer, use: \n   send4me -t {ip_address}")
+            print("Info:   Ready to receive files.")
+            print("Info:   Waiting for connection...")
+            print(f"Info:   To connect and send files, on the other computer, use: send4me -t {ip_address}")
             #print("After 5 minutes, this app will automatically exit")
-            print("Press Ctrl+C if you want to quit immediately\n\n")
+            print("Info:   Press Ctrl+C if you want to quit immediately\n\n")
             #time.sleep(STANDBY_TIME)
             #return False
+        
+
+            conn, addr = s.accept()
         except KeyboardInterrupt:
             print("\n\n")
-            print("Socket has been closed.")
-            print("Thanks for using Send4Me. See you next time\n\n")
+            print("Info:   Socket has been closed.")
+            print("Info:   Thanks for using Send4Me. See you next time\n\n")
             exit(1)
 
-        conn, addr = s.accept()
         with conn:
-            print("Connected by", addr)
+            print("Info:   Connected by", addr)
 
-            print("Now receiving the number of files:")
+            #Receive 8-byte header: the amount of files
             buffer = conn.recv(8)
             l = buffer.decode().split()
             rcv_file_amount = int(l[0])
 
             for i in range(rcv_file_amount):
-                print("Now receiving file header")
+                #print("Now receiving file header")
                 buffer = conn.recv(64)
                 
                 l = buffer.decode().split()
@@ -65,76 +67,97 @@ def receive_content():
                 file_name = l[0]
                 file_size = int(l[1])
 
-                print(file_name, file_size)
-
+                #print(file_name, file_size)
+                print(f"Info:   Receiving file {file_name}")
                 with open(file_name, "wb") as rf:
                     remain_byte = file_size
                     while remain_byte > BUFFER_SIZE:
                         byte_read = conn.recv(BUFFER_SIZE)
                         rf.write(byte_read)
-                        print("Writing to file")
+                        #print("Writing to file")
                         remain_byte -= BUFFER_SIZE
 
                     #Receive the last packet
                     byte_read = conn.recv(remain_byte)
                     rf.write(byte_read)
-                print(f"Finished receiving {file_name}")
-    print("Successfully received one file")
-    print("close the socket")
+                print(f"Info:   {file_name} received succcessfully")
+            print(f"Info:   Successfully received {rcv_file_amount} files")
+    #print("Info:   Closing the socket")
+    print("""
+Info:   Thanks for using Send4Me. See you next time.
+
+
+    """)
+    
 
 def send_files(target_ip, file_list):
-    file_amount = len(file_list)
+    
+    print(f"Info:   Connecting to host at {target_ip} ...")
 
+    file_amount = len(file_list)
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         try:
             s.connect((target_ip, 9587))
-            print("Connect successfle")
+            print("Info:   Connect successful")
         except Exception as e:
-            print(f"Cannot connect to host: {target_ip}")
-            print("Please check the if the IP address specified is correct.")
+            print("-"*76)
+            print(f"Error:  Cannot connect to host: {target_ip}")
+            print("        Please check if send4me is running at the remote host. ")
+            print(f"        And the IP address {target_ip} of the remote host is correct.")
+            print()
+            print()
+
             exit(1)
         
-        
-        print(f"There are {file_amount} files to be sent")
+
+
+        print(f"Info:   There are {file_amount} files to be sent")
         #pack thr file_amount into an 8-byte header
         head_buffer = str(file_amount).encode() + b'\n'
         head_size = len(head_buffer)
         head_buffer += b'0' * ( 8 - head_size )
         s.sendall(head_buffer)
 
-        print("Now opening file to be sent")
+        
         for filename in file_list:
+            print(f"Info:   Open file {filename} ")
             #Get the size of the file, 
             #Server need this number to know when to finish receiving one file
             file_len = os.path.getsize(filename)
             # send out file_to_send
-            with open(filename, "rb") as f:
-                if not f:
-                    print(f"Cannot open file: {filename}")
-                    exit(1)
-                head_buffer = filename.encode() + b'\n'
-                head_buffer += str(file_len).encode() + b'\n'
-                head_size = len(head_buffer)
-                if head_size > 64:
-                    print("File name is too long, or the size is too big, please use other tools to send this file")
-                    exit(1)
-                else:
-                    #Fill the header size to 64 byte
-                    head_buffer += b'0' * ( 64-head_size )
-                #Send the file name and its size, this header is 64 byte long:
+            try:
+                with open(filename, "rb") as f:
+                    head_buffer = filename.encode() + b'\n'
+                    head_buffer += str(file_len).encode() + b'\n'
+                    head_size = len(head_buffer)
+                    if head_size > 64:
+                        print("-"*76)
+                        print("Error:  File name is too long, or the size is too big, please use other tools to send this file")
+                        exit(1)
+                    else:
+                        #Fill the header size to 64 byte
+                        head_buffer += b'0' * ( 64-head_size )
+                    #Send the file name and its size, this header is 64 byte long:
                 
-                s.sendall(head_buffer)
+                    s.sendall(head_buffer)
 
-                print(f"Sending {filename}")
-                remain_size = file_len
-                while remain_size > BUFFER_SIZE:
-                    bytes_read = f.read(BUFFER_SIZE) 
+                    #print(f"Info:   Sending {filename}")
+                    remain_size = file_len
+                    while remain_size > BUFFER_SIZE:
+                        bytes_read = f.read(BUFFER_SIZE) 
+                        s.sendall(bytes_read)
+                        remain_size -= BUFFER_SIZE
+                    bytes_read = f.read(remain_size) 
                     s.sendall(bytes_read)
-                    remain_size -= BUFFER_SIZE
-                bytes_read = f.read(remain_size) 
-                s.sendall(bytes_read)
-                print(f"Finished sending {filename}")
-
+                    print(f"Info:   Finished sending {filename}")
+            except FileNotFoundError:
+                print("-"*76)
+                print(f"Error:  File {filename} is not available")
+                print("        Please check the path and the filename.")
+                exit(1)
+        print(f"Info:   {file_amount} files have been sent successfully.")
+        print("Info:   Thanks for using Send4Me. See you again.")
+        
 def get_file_list(path):
     file_list = []
     for (dirpath, dirnames, filenames) in os.walk(path):
@@ -149,15 +172,32 @@ def get_file_list(path):
     file_list = [x for x in file_list if x[0]!='.']
     return file_list
     
+def check_empty_file_list(fl):
+    if fl == []:
+        print("-"*76)
+        print("Error:  No files to be sent.")
+        print("-"*76)
+        print("          Please specify the files you want to send.")
+        print("          Or copy them here and rerun send4me")
+        print()
+        print()
+        exit(1)
 
 if len(sys.argv)==1: #There is no input argument
     #Get target IP address from configuration file
     target_ip = get_target_ip_address()
     if not target_ip: #Failed in getting saved IP address
-        print("Please specify the target IP address")
-    else:
+        print("-"*76)
+        print("Error:  Cannot decide the target IP address")
+        print("          Please specify the target IP address, for example:")
+        print("          send4me -t 192.168.1.105")
+        print()
+        print()
+    else:        
         #scan and get a list of files in current directory
         file_list = get_file_list('.')
+        check_empty_file_list(file_list)
+        
         #send files
         send_files(target_ip, file_list)
 
@@ -166,17 +206,20 @@ elif sys.argv[1] == "-listen":
 
 elif sys.argv[1] == '-t': #specifying the target ip address
     target_ip = sys.argv[2]
+
     length_argv = len(sys.argv)
     if length_argv > 3: # User specified one or more files to be sent
         file_list = []
         for i in range(3, length_argv):
             file_list.append(sys.argv[i])
-        
+
         send_files(target_ip, file_list)
         
     else: #User did not specify the file to be sent, which mean all files in current folder should be sent
         #scan and get a list of files in current directory
         file_list = get_file_list('.')
+
+        check_empty_file_list
         #send files
         send_files(target_ip, file_list)
 
