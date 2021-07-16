6c858f2a965b592c49757175972bfe77b4d91708
JazzZeng <jazzzeng@LAPTOP-ASUS.localdomain>
2021-07-16 16:54:22 +1000

    Updated output message for test case 01

--- test_case/test_receiver.sh
+++ test_case/test_receiver.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-# Delete all files except for send4me app
-for FILE in *
-do
-    #echo $FILE
-    if [[ ! $FILE =~ ^send4m ]] && [ $FILE != test_receiver.sh ]
-    then
-        echo Deleted $FILE
-        rm $FILE
-    fi
-done
+
+current_dir=$(pwd)
+if [[ ! $current_dir =~ "test_case" ]] 
+then
+    echo It seems like you are not in the "test_case" folder.
+    echo This script is designed to be run in the "test_case" folder
+    exit 1
+fi
+
+if [[ -d receiver ]]
+then
+    rm -r receiver
+fi
+
+mkdir receiver
+cd receiver
+cp ../../send4me.sh .
+cp ../../send4me.py .
 
 # Confirm file testfile1.txt does not exist
 if [[ -s testfile1.txt ]] 
@@ -18,14 +26,20 @@ then
     exit 1
 fi
 
-./send4me.sh -listen
+echo " Running test case 01 for receiver."
+echo " Ready to receive file from the client."
+echo " Please go to the sender computer to run test_sender_01.sh"
+./send4me.sh -listen > test01-receiver-log.txt
 
-if [[ $? == 0 ]]
+if [[ $? != 0 ]]
 then
-    echo Test failed. Please check the error message above.
+    echo "!!!Test failed!!!"
+    echo "You can find the output log in test01-receiver-log.txt"
+    exit 1
 fi
 
 if [[ -s testfile1.txt ]] 
 then
-    echo Successfully received testfile1.txt
+    echo "Test 01 Passed"
+    echo "Successfully received testfile1.txt"
 fi
--- test_case/test_sender.sh
+++ test_case/test_sender.sh
@@ -1,25 +1,60 @@
 #!/bin/bash
-current_dir=$( pwd )
-echo $current_dir
 
-if [[  $current_dir =~ "sender" ]] 
+# Please assign the target IP here
+TARGET_IP="127.0.0.1"
+
+current_dir=$( pwd )
+
+if [[ ! $current_dir =~ "test_case" ]] 
 then
-    echo It seems like you are not in the "sender" folder.
-    echo This script is designed to be run under the "sender" folder
+    echo It seems like you are not in the "test_case" folder.
+    echo This script is designed to be run in the "test_case" folder
+    exit 1
 fi
 
+if [[ ! -d sender ]]
+then
+    mkdir sender
+fi
+
+# First test the testing target is accessible
+echo " Step 1:   Try to ping the target computer @ $TARGET_IP ..."
+echo
+ping $TARGET_IP -c 4 
+if [[ $? != 0 ]]
+then
+    echo "Cannot reach the computer at $TARGET_IP."
+    echo "Please confirm the IP address is correct. You can open this script and edit it in the 4th line"
+    exit 1
+else
+    echo " Step 1 Passed"
+    echo 
+fi
+
+cd sender
+cp ../../send4me.sh .
+cp ../../send4me.py .
+# Create a txt file for transmistion
+env>testfile1.txt
+
+# Check the txt file exists and its size is bigger than 0
 if [[ -s testfile1.txt ]] 
 then
-    echo OKOK
+    echo
+    echo " Step 2: Sending file: testfile1.txt"
 else
-    echo We are going to send testfile1.txt
-    echo Please make sure testfile1.txt exists in the current directory
+    echo "Cannot create testfile1.txt for this test case"
+    exit 1
 fi
 
-./send4me.sh -t 127.0.0.1 testfile1.txt
+./send4me.sh -t $TARGET_IP testfile1.txt > test01-sender-log.txt
 
 if [[ $? == 0 ]]
 then
-    echo File has been sent successfully
-    echo Please check the result on the receiver side.
+    echo " Step 2 Passed."
+    echo " Please check the result on the receiver side to make sure the test case is successful."
+else
+    echo "!!!Test failed!!!"
+    echo "You can find the output log in test01-sender-log.txt"
+    exit 1
 fi
